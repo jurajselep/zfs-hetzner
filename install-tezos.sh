d8c6dab55ddf64207d52fcab4c4c3b57bc19db91
Juraj <jurajselep@gmail.com>
2021-09-27 22:34:59 +0200

    fix

--- install-tezos.sh
+++ install-tezos.sh
@@ -1,4 +1,5 @@
 echo "======= install rust =========="
+cd /home/tezos
 wget https://sh.rustup.rs/rustup-init.sh
 chmod +x rustup-init.sh
 ./rustup-init.sh --profile minimal --default-toolchain 1.52.1 -y
