27833db9bfbff9d27f22e089f618452221a4be0e
Juraj <jurajselep@gmail.com>
2021-09-27 19:50:46 +0200

    Match all for all hostnames

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -344,7 +344,7 @@ function ask_hostname {
 
   local hostname_invalid_message=
 
-  while [[ ! $v_hostname =~ ^[a-z][a-zA-Z0-9\-_:.-]+$ ]]; do
+  while [[ ! $v_hostname =~ [^\n]+ ]]; do
     v_hostname=$(dialog --inputbox "${hostname_invalid_message}Set the host name" 30 100 "$c_default_hostname" 3>&1 1>&2 2>&3)
 
     hostname_invalid_message="Invalid host name! "
