14aea91a38b839b05e1e8a68a18bf9001b05368b
Juraj <jurajselep@gmail.com>
2021-09-27 19:56:35 +0200

    Changed zfs defaults

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -38,10 +38,10 @@ v_suitable_disks=()
 c_deb_packages_repo=http://mirror.hetzner.de/ubuntu/packages
 c_deb_security_repo=http://mirror.hetzner.de/ubuntu/security
 
-c_default_zfs_arc_max_mb=250
+c_default_zfs_arc_max_mb=48000
 c_default_bpool_tweaks="-o ashift=12 -O compression=lz4"
-c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=zstd-9 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD"
-c_default_hostname=terem
+c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=off -O dnodesize=auto -O relatime=off -O xattr=sa -O normalization=formD"
+c_default_hostname=
 c_zfs_mount_dir=/mnt
 c_log_dir=$(dirname "$(mktemp)")/zfs-hetzner-vm
 c_install_log=$c_log_dir/install.log
