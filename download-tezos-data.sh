21e34f2e73d49a3ba2dc51ad0a79dbdc82b25cab
Juraj <jurajselep@gmail.com>
2021-09-29 21:56:07 +0200

    change port

--- download-tezos-data.sh
+++ download-tezos-data.sh
@@ -1,5 +1,5 @@
 echo "======= download tezos data ========"
-rsync --inplace --progress -h -r 94.130.221.4:/data/'*' /data
+rsync --inplace --progress -h -r 95.216.102.157:/data/'*' /data
 
 
 echo "======= generate new node identity ========"
