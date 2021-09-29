59c0ea5d0314fbca86528f90f7fe76f8607ce0a2
Juraj <jurajselep@gmail.com>
2021-09-29 02:50:07 +0200

    add sudo

--- download-tezos-data.sh
+++ download-tezos-data.sh
@@ -2,7 +2,7 @@ echo "======= install zfs snapshots =========="
 wget https://github.com/zfsonlinux/zfs-auto-snapshot/archive/upstream/1.2.4.tar.gz
 tar -xzf 1.2.4.tar.gz
 cd zfs-auto-snapshot-upstream-1.2.4
-make install
+sudo make install
 
 echo "======= create zfs directory =========="
 zfs create rpool/data
