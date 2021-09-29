fa9dcbd56578c0fd976e0b4f21a15c8d995eb24c
Juraj <jurajselep@gmail.com>
2021-09-29 02:56:21 +0200

    move zfs

--- add-user.sh
+++ add-user.sh
@@ -16,3 +16,14 @@ chmod 600 /home/tezos/.ssh/authorized_keys
 echo "======= install tezos =========="
 echo "======= install tezos dependencies =========="
 apt install --yes rsync git m4 build-essential patch unzip wget pkg-config libgmp-dev libev-dev libhidapi-dev libffi-dev opam jq zlib1g-dev autoconf rsync
+
+echo "======= install zfs snapshots =========="
+wget https://github.com/zfsonlinux/zfs-auto-snapshot/archive/upstream/1.2.4.tar.gz
+tar -xzf 1.2.4.tar.gz
+cd zfs-auto-snapshot-upstream-1.2.4
+make install
+
+echo "======= create zfs directory =========="
+zfs create rpool/data
+chown -R tezos:tezos /data
+zfs set recordsize=8k rpool/data
--- download-tezos-data.sh
+++ download-tezos-data.sh
@@ -1,15 +1,7 @@
-echo "======= install zfs snapshots =========="
-wget https://github.com/zfsonlinux/zfs-auto-snapshot/archive/upstream/1.2.4.tar.gz
-tar -xzf 1.2.4.tar.gz
-cd zfs-auto-snapshot-upstream-1.2.4
-sudo make install
 
-echo "======= create zfs directory =========="
-zfs create rpool/data
-zfs set recordsize=8k rpool/data
 
 echo "======= download tezos data ========"
 
 
 echo "======= generate new node identity ========"
-/home/tezos/tezos-node identity generate -d /data
+/home/tezos/tezos/tezos-node identity generate -d /data
