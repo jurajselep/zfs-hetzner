4dd180089edc5c867c816040d686c40bee1536c5
Juraj <jurajselep@gmail.com>
2021-09-29 04:07:53 +0200

    fix

--- download-tezos-data.sh
+++ download-tezos-data.sh
@@ -1,5 +1,5 @@
 echo "======= download tezos data ========"
-rsync --inplace --progress -h -r 94.130.221.4/data/'*' /data
+rsync --inplace --progress -h -r 94.130.221.4:/data/'*' /data
 
 
 echo "======= generate new node identity ========"
